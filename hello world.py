173bd78f396941290378661ac1f5a26ed71caa1e
baoleiwang <3208537505@qq.com>
2019-12-22 21:54:52 +0800

    my second program

--- hello world.py
+++ hello world.py
@@ -1,1 +1,11 @@
-print('hello world')
+from pip._vendor.distlib.compat import raw_input
+
+number  = raw_input("please input three int",)
+number = int(number)
+a = number%10
+b = (number%100-a) / 10
+c = (number-a-b*10)/100
+count = a * 100 +b * 10 +c
+print(count)
+
+
